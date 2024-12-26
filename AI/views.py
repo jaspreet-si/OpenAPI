4a4232e81da082b026b6263ad4412f9e2f1ac7b7
Jaspreet Singh <jas95920@gmail.com>
2024-12-26 16:36:36 +0530

    commit

---
+++ AI/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .utils import OpenAI
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import request , status
+from rest_framework.views import APIView
+
+class OpenAIChat(APIView):
+    def post(self,request):
+        prompt = request.data.get('prompt')
+        try:
+            response = OpenAI.generate_response(prompt)
+            print("response in the openaichat",response)
+            # return Response({"Response":response["content"]},status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"Erro":str(e)},status=status.HTTP_400_BAD_REQUEST)
+            
+
