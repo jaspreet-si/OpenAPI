093c2920dc2246aef3ade808b517b942640da40c
Jaspreet Singh <jas95920@gmail.com>
2024-12-26 17:13:13 +0530

    made changes in the view.py

--- AI/views.py
+++ AI/views.py
@@ -8,11 +8,15 @@ from rest_framework.views import APIView
 class OpenAIChat(APIView):
     def post(self,request):
         prompt = request.data.get('prompt')
-        try:
-            response = OpenAI.generate_response(prompt)
-            print("response in the openaichat",response)
-            # return Response({"Response":response["content"]},status=status.HTTP_201_CREATED)
-        except Exception as e:
-            return Response({"Erro":str(e)},status=status.HTTP_400_BAD_REQUEST)
+        if prompt:  
+            try:
+                response = OpenAI.generate_response(prompt)
+                print("response in the openaichat",response)
+                return Response({"Response":response["content"]},status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({"Erro":str(e)},status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"Erro":"Prompr not provided"},status=status.HTTP_204_NO_CONTENT)
+        
             
 
